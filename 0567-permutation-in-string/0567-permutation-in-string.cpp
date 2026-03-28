16a2bc109400972698b6283e7584ee848d7b6265
Saurav Suman <108250158+SauravRai18@users.noreply.github.com>
2026-03-28 15:46:30 +0530

    Time: 200 ms (22.13%), Space: 23.5 MB (11.48%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    bool issame(vector<int>& freq1, vector<int>& freq2){
+
+        for(int i = 0; i<26; i++){
+
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    bool checkInclusion(string s1, string s2) {
+
+        if(s1.size() > s2.size()) return false;
+
+        vector<int> freq1(26, 0);
+
+        for(auto ch : s1){
+
+            freq1[ch - 'a']++;
+
+        }
+
+        int winSize = s1.size();
+
+        for(int i = 0; i<s2.size(); i++){
+
+            int winIdx = 0;
+            int idx = i;
+
+            vector<int> freq2(26, 0);
+
+            while(winIdx < winSize && idx < s2.size()){
+
+                freq2[s2[idx] - 'a']++;
+                winIdx++;
+                idx++;
+            }
+
+            if(issame(freq1, freq2)){
+
+                return true;
+            }
+            
+        }
+
+        return false;
+
+         
+        
+    }
+};
