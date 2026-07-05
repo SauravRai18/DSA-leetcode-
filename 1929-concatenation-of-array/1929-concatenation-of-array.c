dcf77dc14bda968d45474bd8dca8d8223968191a
Saurav Suman <108250158+SauravRai18@users.noreply.github.com>
2026-07-05 18:59:10 +0530

    Time: 0 ms (100%), Space: 15.2 MB (32.21%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.c
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -3,21 +3,17 @@
  */
 int* getConcatenation(int* nums, int numsSize, int* returnSize) {
 
-    *returnSize = 2 * numsSize;
+    *returnSize = numsSize * 2;
 
-    int *ans = (int *) malloc(*returnSize * sizeof(int));
-
+    int *ans = (int *)malloc((* returnSize) * sizeof(int));
 
     for(int i = 0; i<numsSize; i++){
 
         ans[i] = nums[i];
 
         ans[i + numsSize] = nums[i];
-    } 
-
+    }
 
     return ans;
-
-
     
 }
